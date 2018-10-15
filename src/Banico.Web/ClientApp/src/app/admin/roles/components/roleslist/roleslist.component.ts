620886033ece735c8d9bef3f3238b7f87407e502
cryptokeywords <admin@cryptokeywords.com>
2018-10-16 05:54:36 +1100

    roles list

--- src/Banico.Web/ClientApp/src/app/admin/roles/components/roleslist/roleslist.component.ts
+++ src/Banico.Web/ClientApp/src/app/admin/roles/components/roleslist/roleslist.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { Role } from "../../../../entities/role";
+import { RolesService } from "../../main/roles.service";
 
 @Component({
     selector: 'rolelist',
@@ -8,4 +9,20 @@ import { Role } from "../../../../entities/role";
 })
 export class RolesListComponent {
     public roles: Role[];
+
+    constructor(
+        @Inject(RolesService) public rolesService: RolesService
+    ) {
+    }
+
+    ngOnInit() {
+        this.roles = new Array();
+        this.rolesService.getAll()
+            .subscribe(roles => this.setRoles(roles));
+    }
+
+    private setRoles(roles: Role[])
+    {
+        this.roles = roles;
+    }
 }
--- src/Banico.Web/ClientApp/src/app/entities/role.ts
+++ src/Banico.Web/ClientApp/src/app/entities/role.ts
@@ -1,4 +1,5 @@
 export class Role {
     id: string;
     name: string;
+    decription: string;
   }
