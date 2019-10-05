bf07d42be15faa6f600023052860fa5546dee86f
jasebanico <jason@banico.org>
2019-10-05 11:13:08 +1000

    admin and layout fix

--- src/Banico.Web/ClientApp/app/admin/home/home.component.ts
+++ src/Banico.Web/ClientApp/app/admin/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { ConfigsService } from "../config/services/configs.service";
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
     selector: 'app-admin-home',
@@ -8,19 +9,31 @@ import { ConfigsService } from "../config/services/configs.service";
 })
 export class AdminHomeComponent {
     isInitialized: boolean;
+    configsAllowed: boolean = false;
+    sectionsAllowed: boolean = false;
+    rolesAllowed: boolean = false;
+    usersAllowed: boolean = false;
 
     constructor(
-        private configsService: ConfigsService
+        private configsService: ConfigsService,
+        private authService: AuthService
         ) {
     }
 
     public ngOnInit() {
         this.configsService.initialized()
             .subscribe(result => this.isInitialized = result);
+        this.setPermissions();
     }
 
-    public initialize() {
-        this.configsService.setInitialSettings()
-            .subscribe(result => this.isInitialized = true);
+    private setPermissions() {
+        this.authService.canAccess('admin/configs', '', false)
+            .subscribe(result => this.configsAllowed = result);
+            this.authService.canAccess('admin/sections', '', false)
+            .subscribe(result => this.sectionsAllowed = result);
+            this.authService.canAccess('admin/roles', '', false)
+            .subscribe(result => this.rolesAllowed = result);
+            this.authService.canAccess('admin/users', '', false)
+            .subscribe(result => this.usersAllowed = result);
     }
 }
