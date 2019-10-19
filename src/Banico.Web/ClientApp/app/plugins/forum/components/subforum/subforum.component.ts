0a5f5de96bf8f5cdf72a0f92d313be3d9c505da2
jasebanico <jason@banico.org>
2019-10-19 22:40:53 +1100

    icon fixes

--- src/Banico.Web/ClientApp/app/plugins/forum/components/subforum/subforum.component.ts
+++ src/Banico.Web/ClientApp/app/plugins/forum/components/subforum/subforum.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Subforum } from '../../entities/subforum';
-import { ForumSubforumService } from '../../services/subforum.service';
-import { ForumTopicService } from '../../services/topic.service';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Topic } from '../../entities/topic';
-import { SubforumEntityService } from '../../services/subforum-entity.service';
+import { Component, OnInit } from "@angular/core";
+import { Subforum } from "../../entities/subforum";
+import { ForumSubforumService } from "../../services/subforum.service";
+import { ForumTopicService } from "../../services/topic.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { Topic } from "../../entities/topic";
+import { SubforumEntityService } from "../../services/subforum-entity.service";
 
 @Component({
-  selector: 'app-plugins-forum-subforum',
-  templateUrl: './subforum.component.html',
-  styleUrls: ['./subforum.component.scss']
+  selector: "app-plugins-forum-subforum",
+  templateUrl: "./subforum.component.html",
+  styleUrls: ["./subforum.component.scss"]
 })
 export class ForumSubforumComponent implements OnInit {
   //public subforum$: Observable<Subforum>;
@@ -21,26 +21,29 @@ export class ForumSubforumComponent implements OnInit {
     //private subforumEntityService: SubforumEntityService,
     private subforumService: ForumSubforumService,
     private topicService: ForumTopicService,
-    private route: ActivatedRoute
-    ) {
-  }
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      if (params['alias']) {
-        var alias = params['alias'];
-        this.subforumService.getByAlias(alias)
-          .subscribe(subforum => {
-            this.set(subforum);
-          });
-        }
+      if (params["alias"]) {
+        var alias = params["alias"];
+        this.subforumService.getByAlias(alias).subscribe(subforum => {
+          this.set(subforum);
+        });
+      }
     });
   }
 
   private set(subforum: Subforum) {
     this.subforum = subforum;
-    this.topicService.getTopics(subforum.id)
-      .subscribe(topics => this.topics = topics);
+    this.topicService
+      .getTopics(subforum.id)
+      .subscribe(topics => (this.topics = topics));
   }
 
+  public edit() {
+    this.router.navigate(["/forum/edit/" + this.subforum.id]);
+  }
 }
