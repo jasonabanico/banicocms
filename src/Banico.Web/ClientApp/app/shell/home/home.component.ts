6c25b35f9c9cf05373eeba33b1fc8bf6160de24a
jasebanico <jason@banico.org>
2019-10-26 12:19:18 +1100

    home page fix

--- src/Banico.Web/ClientApp/app/shell/home/home.component.ts
+++ src/Banico.Web/ClientApp/app/shell/home/home.component.ts
@@ -1,25 +1,38 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import {Router} from '@angular/router';
+import { Component, OnInit, Inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { AppConfig } from "../../../../Config/app.config";
 
 @Component({
-    selector: 'app-shell-home',
-    templateUrl: '../../../../config/home.component.html'
+  selector: "app-shell-home",
+  templateUrl: "../../../../config/home.component.html"
 })
 export class HomeComponent implements OnInit {
-    public location: string;
-    public content: string;
+  public appTitle: string;
+  public frontText1: string;
+  public frontText2: string;
+  public startButtonUrl: string;
+  public startButtonText: string;
+  public location: string;
+  public content: string;
 
-    readonly SECTION_DELIM: string = '*';
+  readonly SECTION_DELIM: string = "*";
 
-    constructor(private router: Router) {
-    }
+  constructor(private router: Router) {
+    this.appTitle = AppConfig.APP_NAME;
+    this.frontText1 = AppConfig.FRONT_TEXT_1;
+    this.frontText2 = AppConfig.FRONT_TEXT_2;
+    this.startButtonUrl = AppConfig.START_BUTTON_URL;
+    this.startButtonText = AppConfig.START_BUTTON_TEXT;
+  }
 
-    ngOnInit() {
-        this.location = '';
-        this.content = '';
-    }
+  ngOnInit() {
+    this.location = "";
+    this.content = "";
+  }
 
-    public search() {
-        this.router.navigateByUrl('directory/search/' + this.content + this.SECTION_DELIM + this.location);
-    }
+  public search() {
+    this.router.navigateByUrl(
+      "directory/search/" + this.content + this.SECTION_DELIM + this.location
+    );
+  }
 }
--- src/Banico.Web/config/app.config.ts
+++ src/Banico.Web/config/app.config.ts
@@ -1,6 +1,13 @@
 ﻿export class AppConfig {
   public static APP_NAME = "Banico";
-  public static END_PAGE_TITLE = "Content, Collaboration, and Community";
+  public static END_PAGE_TITLE =
+    "Content, Communication, Collaboration, Commerce, and Community";
   public static DEFAULT_PAGE_TITLE = "Banico";
   public static GOOGLE_MAP_API_KEY = "** GOOGLE MAP API KEY **";
+
+  public static FRONT_TEXT_1 =
+    "Banico CMS is a lean modular system for building content, communication, collaboration, commerce, and community platforms, with ASP.Net Core, Angular, and Bootstrap.";
+  public static FRONT_TEXT_2 = "Explore the features of Banico CMS.";
+  public static START_BUTTON_TEXT = "Learn More";
+  public static START_BUTTON_URL = "https://github.com/jasebanico/banicocms";
 }
