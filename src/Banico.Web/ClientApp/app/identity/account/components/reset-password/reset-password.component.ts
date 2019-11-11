e91732e901ce0acad98911e91b8e6b7416614f46
jasebanico <jason@banico.org>
2019-11-11 21:02:07 +1100

    reset password layout

--- src/Banico.Web/ClientApp/app/identity/account/components/reset-password/reset-password.component.ts
+++ src/Banico.Web/ClientApp/app/identity/account/components/reset-password/reset-password.component.ts
@@ -1,66 +1,62 @@
-import {finalize} from 'rxjs/operators';
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { NgForm, FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { AccountService } from '../../services/account.service';
- 
+import { finalize } from "rxjs/operators";
+import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { NgForm, FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { AccountService } from "../../services/account.service";
+
 @Component({
-  selector: 'app-identity-account-reset-password',
-  templateUrl: './reset-password.component.html',
+  selector: "app-identity-account-reset-password",
+  templateUrl: "./reset-password.component.html",
   styleUrls: []
 })
 export class IdentityAccountResetPasswordComponent {
   private sub: any;
   public isRequesting: boolean;
-  public isSuccessful: boolean;
+  public isSuccessful: boolean = false;
   public errors: string;
 
   public resetPasswordForm: FormGroup = this.fb.group({
-    email: ['', Validators.required],
-    code: ['', Validators.required],
-    password: ['', Validators.required],
-    confirmPassword: ['', Validators.required]
+    email: ["", Validators.required],
+    code: ["", Validators.required],
+    password: ["", Validators.required],
+    confirmPassword: ["", Validators.required]
   });
-  
+
   constructor(
     private accountService: AccountService,
     private router: Router,
     private route: ActivatedRoute,
     private fb: FormBuilder
-  ) { }
-  
+  ) {}
+
   ngOnInit() {
-    this.sub = this.route.queryParams
-      .subscribe(params => {
-        var email = params.email;
-        var code = params.code;
-        if (email) {
-          this.resetPasswordForm.controls['email'].setValue(email);
-          this.resetPasswordForm.controls['code'].setValue(code);
-        }
-      });
+    this.sub = this.route.queryParams.subscribe(params => {
+      var email = params.email;
+      var code = params.code;
+      if (email) {
+        this.resetPasswordForm.controls["email"].setValue(email);
+        this.resetPasswordForm.controls["code"].setValue(code);
+      }
+    });
   }
-  
+
   public resetPassword() {
     this.isRequesting = true;
-    this.accountService.resetPassword(
-      this.resetPasswordForm.value['email'],
-      this.resetPasswordForm.value['code'],
-      this.resetPasswordForm.value['password'],
-      this.resetPasswordForm.value['confirmPassword']
-    ).pipe(
-    finalize(() => this.isRequesting = false))
-    .subscribe(
-      result  => {if (result){
-          this.router.navigate(
-            ['/login'], {
-              queryParams: {
-                brandNew: true,
-                email: this.resetPasswordForm.value['email']
-              }
-            }
-          );                         
-      }},
-      errors =>  this.errors = errors);
+    this.accountService
+      .resetPassword(
+        this.resetPasswordForm.value["email"],
+        this.resetPasswordForm.value["code"],
+        this.resetPasswordForm.value["password"],
+        this.resetPasswordForm.value["confirmPassword"]
+      )
+      .pipe(finalize(() => (this.isRequesting = false)))
+      .subscribe(
+        result => {
+          if (result) {
+            this.isSuccessful = true;
+          }
+        },
+        errors => (this.errors = errors)
+      );
   }
 }
