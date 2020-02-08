d6ebb473d27ae789edad60b436891e08c51a50b2
jasebanico <jason@banico.org>
2020-02-08 22:21:30 +1100

    robots and meta tag for SEO

--- src/Banico.Web/ClientApp/app/app.component.ts
+++ src/Banico.Web/ClientApp/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { Title } from "@angular/platform-browser";
+import { Title, Meta } from "@angular/platform-browser";
 import { AppConfig } from "../../Config/app.config";
 
 @Component({
@@ -10,7 +10,8 @@ import { AppConfig } from "../../Config/app.config";
 export class AppComponent {
   title = "app";
 
-  public constructor(private titleService: Title) {
+  public constructor(private titleService: Title, private meta: Meta) {
     this.titleService.setTitle(AppConfig.APP_NAME);
+    this.meta.addTag({ name: 'description', content: AppConfig.FRONT_TEXT_1 + " " + AppConfig.FRONT_TEXT_2 });
   }
 }
