8da3c47fce7294734c5dcc7b799b0d2ef05f29c8
jasebanico <jason@banico.org>
2020-03-07 15:22:47 +1100

    embed youtube imgur instagram

--- src/Banico.Web/ClientApp/app/shell/embed/embed.component.ts
+++ src/Banico.Web/ClientApp/app/shell/embed/embed.component.ts
@@ -12,12 +12,38 @@ export class EmbedComponent {
 
   constructor(public sanitizer: DomSanitizer) {}
 
+  private embeds = [
+    {
+      site: "youtube",
+      regex:
+        "(?:https?:/{2})?(?:w{3}.)?youtu(?:be)?.(?:com|be)(?:/watch?v=|/)([^s&]+)",
+      index: 1
+    },
+    {
+      site: "imgur",
+      regex:
+        "(?:https?:/{2})?(?:w{3}.)?imgur.com/(gallery|a)/(.*?)(?:[#/].*|$)",
+      index: 2
+    },
+    {
+      site: "instagram",
+      regex: "(?:https?:/{2})?(?:w{3}.)?instagram.com/p/(.*?)(?:[#/].*|$)",
+      index: 1
+    }
+  ];
+
   @Input()
   set tagId(tagId: string) {
     tagId = tagId.replace("{{", "");
     tagId = tagId.replace("}}", "");
-    const values = tagId.split(":");
-    this.tag = values[0];
-    this.id = values[1];
+
+    for (let i = 0; i < this.embeds.length; i++) {
+      const embed = this.embeds[i];
+      let match = tagId.match(embed.regex);
+      if (match) {
+        this.tag = embed.site;
+        this.id = match[embed.index];
+      }
+    }
   }
 }
