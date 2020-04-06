910f0f0e32365d144e1d345e266a966883af64da
jasebanico <jason@banico.org>
2020-04-06 19:11:21 +1000

    disabling ssr

--- src/Banico.Web/Server/Controllers/HomeController.cs
+++ src/Banico.Web/Server/Controllers/HomeController.cs
@@ -22,18 +22,18 @@ namespace Banico.Web.Controllers
 
         [HttpGet]
         public async Task<IActionResult> Index () {
-            var prerenderResult = await this.Request.BuildPrerender ();
+            // var prerenderResult = await this.Request.BuildPrerender ();
 
-            this.ViewData["SpaHtml"] = prerenderResult.Html; // our <app-root /> from Angular
-            this.ViewData["Title"] = prerenderResult.Globals["title"]; // set our <title> from Angular
-            this.ViewData["Styles"] = prerenderResult.Globals["styles"]; // put styles in the correct place
-            this.ViewData["Scripts"] = prerenderResult.Globals["scripts"]; // scripts (that were in our header)
-            this.ViewData["Meta"] = prerenderResult.Globals["meta"]; // set our <meta> SEO tags
-            this.ViewData["Links"] = prerenderResult.Globals["links"]; // set our <link rel="canonical"> etc SEO tags
-            this.ViewData["TransferData"] = prerenderResult.Globals["transferData"]; // our transfer data set to window.TRANSFER_CACHE = {};
-            if (!this.HostingEnvironment.IsDevelopment ()) {
-                this.ViewData["ServiceWorker"] = "<script>'serviceWorker'in navigator&&navigator.serviceWorker.register('/serviceworker')</script>";
-            }
+            // this.ViewData["SpaHtml"] = prerenderResult.Html; // our <app-root /> from Angular
+            // this.ViewData["Title"] = prerenderResult.Globals["title"]; // set our <title> from Angular
+            // this.ViewData["Styles"] = prerenderResult.Globals["styles"]; // put styles in the correct place
+            // this.ViewData["Scripts"] = prerenderResult.Globals["scripts"]; // scripts (that were in our header)
+            // this.ViewData["Meta"] = prerenderResult.Globals["meta"]; // set our <meta> SEO tags
+            // this.ViewData["Links"] = prerenderResult.Globals["links"]; // set our <link rel="canonical"> etc SEO tags
+            // this.ViewData["TransferData"] = prerenderResult.Globals["transferData"]; // our transfer data set to window.TRANSFER_CACHE = {};
+            // if (!this.HostingEnvironment.IsDevelopment ()) {
+            //     this.ViewData["ServiceWorker"] = "<script>'serviceWorker'in navigator&&navigator.serviceWorker.register('/serviceworker')</script>";
+            // }
 
             return View ();
         }
